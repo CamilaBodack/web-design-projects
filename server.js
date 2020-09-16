908872406fb3a48eed9bfb4af88f6cdd7bc95e00
CamilaBodack <camila.bodack@gmail.com>
2020-09-16 11:39:48 -0300

    mapeia os arquivos estaticos no express

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const port = 3000
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.get('/', (req, res) =>{
-	res.send('Servidor funcionando')
-})
+app.use('/tribute_page', express.static('tribute_page'))
+app.use('/survey_form', express.static('survey_form'))
+app.use('/landing', express.static('product_landing_page'))
+app.use('/doc', express.static('technical_documentation_page'))
+app.use('/portifolio', express.static('personal_portifolio'))
 
 app.listen(port, () => {
 	console.log(`Application listening at http://localhost:${port}`)
